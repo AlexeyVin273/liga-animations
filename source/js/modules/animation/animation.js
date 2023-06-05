995e620130b24f25f4d89b79624af4552ac1eb03
Gentlecat-star <81353321+Gentlecat-star@users.noreply.github.com>
2023-06-05 19:50:57 +0600

    Добавляет разметку для стики слайдера

--- source/js/modules/animation/animation.js
+++ source/js/modules/animation/animation.js
@@ -1,11 +1,13 @@
 import {initParallax} from './parallax';
 import {initSplitting} from './splitting';
 import {initAppearance} from './appearance';
+import {initScrollSlider} from './scroll-slider';
 
 const initAnimations = () => {
   initSplitting();
   initParallax();
   initAppearance();
+  initScrollSlider();
 };
 
 export {initAnimations};
---
+++ source/js/modules/animation/scroll-slider.js
@@ -0,0 +1,10 @@
+import {gsap} from '../../vendor/gsap.min.js';
+import {ScrollTrigger} from '../../vendor/ScrollTrigger.min.js';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const initScrollSlider = () => {
+
+};
+
+export {initScrollSlider};
